2a1b07bd6c131c67a09d5531cb5108d85f1bfe7e
LiYi <yliee@foxmail.com>
2019-04-21 12:01:52 +0800

    update

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import os
 
 print(os.path.dirname(os.path.abspath(__file__)))
 
-'''本地调试需要黄建main.py文件'''
+
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 execute(["scrapy","crawl","rengongzhineng"])
